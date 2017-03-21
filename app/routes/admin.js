be4a2268f2edb8c37b65fe597a924f1fcb695fb5
Derek Villars and Sam Kirsch <derek@>
2017-03-21 15:05:59 -0700

    create a delete method

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,16 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll("article");
   },
-
   actions: {
     saveArticle3(params) {
       var newArticle = this.store.createRecord('article', params);
       newArticle.save();
       this.transitionTo('admin');
     },
-    destroyArtical(artical) {
-      article.destroyRecord();
-      this.transitionTo('index');
-    }
   }
 });
--- app/routes/article.js
+++ app/routes/article.js
@@ -3,14 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('article', params.article_id);
+  }, 
+  actions: {
+    destroyArticle(model) {
+      model.destroyRecord();
+      this.transitionTo('index');
+    }
   }
-  // actions: {
-  //   updateArticle(params) {
-  //     for (key in Object.keys(params)) {
-  //       if (params[key]) {
-  //         article.set(key, params[key]);
-  //       }
-  //     };
-  //   }
-  // }
 });
